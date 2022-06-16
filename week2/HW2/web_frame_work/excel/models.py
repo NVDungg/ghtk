ca74f755ece65cffcea37e7ecd3812f9b59641c9
NVDungg <103346233+NVDungg@users.noreply.github.com>
2022-06-16 21:06:54 +0700

    Had a idea not try yet

--- week2/HW2/web_frame_work/excel/models.py
+++ week2/HW2/web_frame_work/excel/models.py
@@ -25,3 +25,36 @@ class Student(models.Model):
 
     def __str__(self):
         return self.name
+    
+    #Another way to try. It split that to 2 models.(Not test yet)
+    
+"""class Student(models.Model):
+    school = models.CharField(max_length=100)
+    district = models.CharField(max_length=100)
+    student_id = models.CharField(max_length=50, unique= True, error_messages={'unique':"This email has already been registered."})
+    stu_class = models.CharField(max_length=30)
+    name = models.CharField(max_length=100)
+    date = models.CharField(max_length=200)
+    sex = models.CharField(max_length=20)
+    place_of_birth = models.CharField(max_length=100)
+    ethnic = models.CharField(max_length=50)
+    location = models.CharField(max_length=255)
+    phone_number = models.IntegerField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class scores(models.Model):
+    student = models.ForeignKey(Student,related_name='studentid',null=True,blank=True,on_delete=models.CASCADE)
+    total_grade_1 = models.PositiveSmallIntegerField(blank=True)
+    total_grade_2 = models.PositiveSmallIntegerField(blank=True)
+    total_grade_3 = models.PositiveSmallIntegerField(blank=True)
+    total_grade_4 = models.PositiveSmallIntegerField(blank=True)
+    total_grade_5 = models.PositiveSmallIntegerField(blank=True)
+    total_5_years = models.PositiveSmallIntegerField(blank=True)
+    plus = models.PositiveSmallIntegerField(blank=True, null= True, default=0)
+    total_all = models.PositiveSmallIntegerField(blank=True, null=True)
+    decripsion = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.student"""
