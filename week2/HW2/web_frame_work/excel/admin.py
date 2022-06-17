14163b52211ce6b9d8acd6e4ef2d2b2e58751081
NVDungg <103346233+NVDungg@users.noreply.github.com>
2022-06-17 16:02:07 +0700

    add description

--- week2/HW2/web_frame_work/excel/admin.py
+++ week2/HW2/web_frame_work/excel/admin.py
@@ -5,4 +5,4 @@ from .models import Student
 
 @admin.register(Student)
 class StudentAdmin(ImportExportActionModelAdmin):
-    list_display = ('student_id','name','sex','location','phone_number','total_all')
+    list_display = ('student_id','name','sex','location','phone_number','total_all')    #displat detail field in local admin
