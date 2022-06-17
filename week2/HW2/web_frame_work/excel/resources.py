eb0ce90513075241e23116d51fabded5764a9477
NVDungg <103346233+NVDungg@users.noreply.github.com>
2022-06-17 16:05:37 +0700

    description

--- week2/HW2/web_frame_work/excel/resources.py
+++ week2/HW2/web_frame_work/excel/resources.py
@@ -1,6 +1,6 @@
 from import_export import resources
 from .models import Student
 
-class StudentResource(resources.ModelResource):
+class StudentResource(resources.ModelResource):     #use lib django-import-export to take model
     class Meta:
         model = Student
